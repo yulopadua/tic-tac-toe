213b8c37a89110832968e5b605ac55cef6013a49
yulopadua <yulo.padua@gmail.com>
2022-06-27 18:16:29 +0800

    Add function to check for winner

--- script.js
+++ script.js
@@ -83,6 +83,51 @@ let displayControllerModule = (function() {
             index++;
             })
 
+            //Run local function to check for win/disable gameBoard from further play/display winner on DOM
+            function checkWin(player) {
+                const horizontal = [0,3,6].map(i=>{return[i,i+1,i+2]});
+                const vertical = [0,1,2].map(i=>{return[i,i+3,i+6]});
+                const diagonal = [[0,4,8],[2,4,6]];
+
+                let allwins = [].concat(horizontal).concat(vertical).concat(diagonal);
+
+                let results = allwins.some(indices => {
+                    return gridBoxes[indices[0]].textContent == player && gridBoxes[indices[1]].textContent == player && gridBoxes[indices[2]].textContent == player})
+                return results;
+            }
+
+            if (checkWin("X") == true) {
+                const body = document.querySelector("body");
+                const playerWinMessage = document.createElement("h1");
+                playerWinMessage.textContent = (gameBoardModule.playerArray[0] + " wins!");
+                body.appendChild(playerWinMessage);
+                makeMove.forEach(makeMoves => {
+                    makeMoves.remove();
+                });
+                startGameButton.remove();
+                return;
+            } else if (checkWin("O") == true) {
+                const body = document.querySelector("body");
+                const playerWinMessage = document.createElement("h1");
+                playerWinMessage.textContent = (gameBoardModule.playerArray[3] + " wins!");
+                body.appendChild(playerWinMessage);
+                makeMove.forEach(makeMoves => {
+                    makeMoves.remove();
+                });
+                startGameButton.remove();
+                return;
+            } else if (gameBoardModule.gameBoard.length == 9) {
+                const body = document.querySelector("body");
+                const playerWinMessage = document.createElement("h1");
+                playerWinMessage.textContent = ("Tie!");
+                body.appendChild(playerWinMessage);
+                makeMove.forEach(makeMoves => {
+                    makeMoves.remove();
+                });
+                startGameButton.remove();
+                return;
+            }
+
         gameBoardModule.makePlayerMove();
         }
     index++;
@@ -92,7 +137,14 @@ let displayControllerModule = (function() {
     const startGameButton = document.querySelector(".start-game-button");
     startGameButton.addEventListener("click", createPlayer);
 
-    return {};
+    //Listen for click to restart the game
+    const clearBoardButton = document.querySelector(".clear-board-button");
+    clearBoardButton.addEventListener("click", clearBoard);
+
+    function clearBoard() {
+        location.reload();
+    }
+
 })();
 
 //Setting up the render module for array display to gameboard
